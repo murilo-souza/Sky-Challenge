5eaf42bd1d4268d207ce0c7f1ca2a9c5989a213a
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-05-06 16:37:03 -0300

    feat: add route to find a unique movie

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -40,7 +40,7 @@ export async function moviesRoutes(app: FastifyInstance) {
         throw new Error('Os dados recebidos não são um array de filmes.')
       }
 
-      const filmesSimplificados: ParsedMovie[] = data.filmes.map((filme) => {
+      const parsedMovies: ParsedMovie[] = data.filmes.map((filme) => {
         const duracaoSegundos = filme.duracao * 60 // Converte minutos para segundos
         const notaIMDb =
           filme.ratings.find((rating) => rating.fonte === 'IMDb')?.valor || 0
@@ -67,7 +67,56 @@ export async function moviesRoutes(app: FastifyInstance) {
         }
       })
 
-      res.send(filmesSimplificados)
+      res.send(parsedMovies)
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Erro ao chamar a API')
+    }
+  })
+
+  app.get('/:name', async (req, res) => {
+    try {
+      const { data } = await axios.get<Movie>(
+        'https://tv5hn2gvyijpl76yxlmsy66jwa0nlmxn.lambda-url.us-east-1.on.aws/',
+      )
+      const { name } = req.params
+
+      const movieByName = data.filmes.find(
+        (filme) => filme.titulo.toLowerCase() === name.toLowerCase(),
+      )
+
+      if (!movieByName) {
+        return res.code(404).send({ message: 'Filme não encontrado.' })
+      }
+
+      const duracaoSegundos = movieByName!.duracao * 60 // Converte minutos para segundos
+      const notaIMDb =
+        movieByName.ratings.find((rating) => rating.fonte === 'IMDb')?.valor ||
+        0
+      const lucro = subtractValue(movieByName.orcamento, movieByName.bilheteria)
+      const maiorPremiacao = movieByName.premios.reduce((maior, premio) => {
+        return premio.relevancia > maior.relevancia ? premio : maior
+      }).nome
+      const sinopse =
+        movieByName.sinopse.find((s) => s.idioma === 'pt-br')?.texto ||
+        movieByName.sinopse.find((s) => s.idioma === 'en')?.texto ||
+        movieByName.sinopse.find(
+          (s) => s.idioma !== 'en' && s.idioma !== 'pt-br',
+        )?.texto
+
+      const parsedMovie: ParsedMovie = {
+        titulo: movieByName.titulo,
+        ano: movieByName.ano,
+        diretor: movieByName.diretor,
+        genero: movieByName.genero,
+        duracaoSegundos,
+        notaIMDb,
+        lucro,
+        maiorPremiacao,
+        sinopse,
+      }
+
+      res.send(parsedMovie)
     } catch (error) {
       console.error(error)
       res.status(500).send('Erro ao chamar a API')
